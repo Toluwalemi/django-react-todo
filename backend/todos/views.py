e0f79c70e73d5d87decbd861afb02bd1aa7d3da9
t.oluwadare <t.oluwadare@innovectives.com>
2021-01-26 23:15:54 +0100

    initial commit

---
+++ backend/todos/views.py
@@ -0,0 +1,10 @@
+# Create your views here.
+from rest_framework import viewsets
+
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
