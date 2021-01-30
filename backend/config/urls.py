fcdc772b02ceaf6cd182adfa401e7c53d6271071
t.oluwadare <t.oluwadare@innovectives.com>
2021-01-30 16:08:35 +0100

    fix bugs

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -20,7 +20,7 @@ from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('todos.urls')),
-    path('api/user/', include('users.urls', namespace='users')),
-    path('token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),  # override sjwt stock token
-    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/user/', include('users.urls')),
+    path('api/token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),  # override sjwt stock token
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import CustomUserCreate
 
-app_name = 'users'
+from .views import CustomUserCreate
 
 urlpatterns = [
     path('create/', CustomUserCreate.as_view(), name="create_user")
