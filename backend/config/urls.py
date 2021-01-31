088a24816012bc8fe9f798369110c3f3bba08f29
t.oluwadare <t.oluwadare@innovectives.com>
2021-01-31 13:18:22 +0100

    fix urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('todos.urls')),
     path('api/user/', include('users.urls')),
-    path('api/token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),  # override sjwt stock token
+    path('api/token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
